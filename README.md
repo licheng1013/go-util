e3190f0209fe69b102f19c139a9fde0c96a132f0
licheng1013 <2501093733@qq.com>
2022-08-13 19:02:49 +0800

    优化文档！

--- README.md
+++ README.md
@@ -10,8 +10,10 @@
 - 中版本迭代
 - v0.0.0 > v0.1.0 增加新工具api
 - 大版本迭代
-- v0.0.0 > v1.0.0 删除废弃的api 
+- v0.0.0 > v1.0.0 删除废弃的api
 
+## 未来如何升级
+- 升级大版本前的最后一个版本然后修改所有使用过期方法的地方在升级到大版本
 
 ## 安装教程
 - Github安装
@@ -27,7 +29,22 @@ require (
 replace go-util => gitee.com/licheng1013/go-util v0.0.4
 ```
 
+## 工具类
+- 废弃的方法不会在表格内显示
 
-## file_util
+### file_util
 - 对文件进行的处理
+
+### gin_util
+- 对gin框架的封装
+- 在一个请求上下文中注入gin的context即可使用：例如在拦截器时候注入
+
+| 方法名             | 作用                |
+|-----------------|-------------------|
+| GinUrlPath      | 获取段路径 /xx/hh      |
+| GinParamsUrlPah | 获取全路径 /xx/nn?xx=a |
+| GinGetContext   | 获取上下文             |
+| GinSetContext   | 设置上下文             |
+
+
 
--- util/gin_util.go
+++ util/gin_util.go
@@ -16,21 +16,38 @@ var GinUtil = context.Background()
 
 const GinUtilKey = "GinUtilKey"
 
-// GetUrlPath 不带参数的  /user/test?name=xx => /user/test
+// GetUrlPath 不带参数的  /user/test?name=xx => /user/test 请使用： GinUrlPath
+// Deprecated
 func GetUrlPath() string {
-	split := strings.Split(GetParamsUrlPah(), "?")
+	return GinUrlPath()
+}
+
+// GinUrlPath 不带参数的  /user/test?name=xx => /user/test
+func GinUrlPath() string {
+	split := strings.Split(GinParamsUrlPah(), "?")
 	path := split[0]
 	return path
 }
 
-// GetParamsUrlPah 带参数的  /user/test?name=xx
+// GetParamsUrlPah 带参数的  /user/test?name=xx 请使用： GinParamsUrlPah
+// Deprecated
 func GetParamsUrlPah() string {
-	c := GetGinContext()
+	return GinParamsUrlPah()
+}
+
+// GinParamsUrlPah 带参数的  /user/test?name=xx
+func GinParamsUrlPah() string {
+	c := GinGetContext()
 	return c.Request.RequestURI
 }
 
-// GetGinContext 获取 GinContext 上下文
+// GetGinContext 获取 GinContext 上下文 请使用： GinGetContext
+// Deprecated
 func GetGinContext() *gin.Context {
+	return GinGetContext()
+}
+
+func GinGetContext() *gin.Context {
 	value := GinUtil.Value(GinUtilKey)
 	if value == nil {
 		panic("没有在gin环境注册！")
@@ -38,7 +55,13 @@ func GetGinContext() *gin.Context {
 	return value.(*gin.Context)
 }
 
-// SetGinContext 设置 GinContext 上下文
+// SetGinContext 设置 GinContext 上下文  请使用： GinSetContext
+// Deprecated
 func SetGinContext(c *gin.Context) {
+	GinSetContext(c)
+}
+
+// GinSetContext 设置 GinContext 上下文
+func GinSetContext(c *gin.Context) {
 	GinUtil = context.WithValue(GinUtil, GinUtilKey, c)
 }
