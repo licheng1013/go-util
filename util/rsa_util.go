9022fa0a138e3489bd1e4dad9981cf34a6c32d3a
licheng1013 <2501093733@qq.com>
2022-08-18 21:08:13 +0800

    清理代码！

--- util/rsa_util.go
+++ util/rsa_util.go
@@ -4,11 +4,6 @@
  *                         http://www.apache.org/licenses/
  */
 
-/*
- *                                  Apache License
- *                            Version 2.0, January 2004
- *                         http://www.apache.org/licenses/
- */
 
 package util
 
