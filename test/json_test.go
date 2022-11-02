5ac8bda4ad008a056477b527ebe39530974401ad
licheng1013 <2501093733@qq.com>
2022-11-02 18:14:06 +0800

    增加json map转换到实体方法！

--- test/json_test.go
+++ test/json_test.go
@@ -5,7 +5,7 @@
 package test
 
 import (
-	"gitee.com/licheng1013/go-util/util"
+	"gitee.com/licheng1013/go-util/common"
 	"testing"
 )
 
@@ -16,16 +16,16 @@ type Hello struct {
 
 func TestJson(t *testing.T) {
 	var m = map[string]string{"A": "b"}
-	str := util.JsonToStr(m)
+	str := common.JsonUtil.JsonToStr(m)
 	t.Log(str)
 	var v map[string]string
-	util.JsonToObj(str, &v)
+	common.JsonUtil.JsonToObj(str, &v)
 	t.Log(v)
 }
 
 func TestStruct(t *testing.T) {
 	m := map[string]interface{}{"age": 1, "name": "hi"}
 	var v Hello
-	util.MapToObj(m, &v)
+	common.JsonUtil.MapToObj(m, &v)
 	t.Log(v)
 }
--- util/json_util.go
+++ util/json_util.go
@@ -5,8 +5,16 @@ import (
 	"github.com/mitchellh/mapstructure"
 )
 
+// JsonUtil json转换工具类
+type JsonUtil struct {
+}
+
+func NewJsonUtil() *JsonUtil {
+	return &JsonUtil{}
+}
+
 // JsonToStr 把对象转换Json字符串！
-func JsonToStr(v interface{}) string {
+func (JsonUtil) JsonToStr(v interface{}) string {
 	if bytes, err := json.Marshal(v); err != nil {
 		panic(err)
 	} else {
@@ -15,14 +23,14 @@ func JsonToStr(v interface{}) string {
 }
 
 // JsonToObj Json转换成对象 => 传参需要带&号 示例=> (jsonStr,&user)
-func JsonToObj(jsonStr string, v interface{}) {
+func (JsonUtil) JsonToObj(jsonStr string, v interface{}) {
 	if err := json.Unmarshal([]byte(jsonStr), v); err != nil {
 		panic(err)
 	}
 }
 
 // MapToObj map转换为结构体，需要注意的是，转换的字段必须大写开头
-func MapToObj(in interface{}, out interface{}) {
+func (JsonUtil) MapToObj(in interface{}, out interface{}) {
 	err := mapstructure.Decode(in, out)
 	if err != nil {
 		panic(err)
