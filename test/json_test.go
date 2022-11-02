dd1c6e409e441e54e298938ddf5ad40b6987b704
licheng1013 <2501093733@qq.com>
2022-11-02 18:11:37 +0800

    增加map转换到结构体！

--- test/json_test.go
+++ test/json_test.go
@@ -9,11 +9,23 @@ import (
 	"testing"
 )
 
+type Hello struct {
+	Name string
+	Age  int
+}
+
 func TestJson(t *testing.T) {
 	var m = map[string]string{"A": "b"}
 	str := util.JsonToStr(m)
 	t.Log(str)
 	var v map[string]string
 	util.JsonToObj(str, &v)
+	t.Log(v)
+}
+
+func TestStruct(t *testing.T) {
+	m := map[string]interface{}{"age": 1, "name": "hi"}
+	var v Hello
+	util.MapToObj(m, &v)
 	t.Log(v)
 }
--- util/json_util.go
+++ util/json_util.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"encoding/json"
+	"github.com/mitchellh/mapstructure"
 )
 
 // JsonToStr 把对象转换Json字符串！
@@ -16,6 +17,14 @@ func JsonToStr(v interface{}) string {
 // JsonToObj Json转换成对象 => 传参需要带&号 示例=> (jsonStr,&user)
 func JsonToObj(jsonStr string, v interface{}) {
 	if err := json.Unmarshal([]byte(jsonStr), v); err != nil {
+		panic(err)
+	}
+}
+
+// MapToObj map转换为结构体，需要注意的是，转换的字段必须大写开头
+func MapToObj(in interface{}, out interface{}) {
+	err := mapstructure.Decode(in, out)
+	if err != nil {
 		panic(err)
 	}
 }
